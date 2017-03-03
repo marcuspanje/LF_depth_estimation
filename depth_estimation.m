7ddb9de8af46ac16e7a596f6c536481b836e2e12
marcuspanje <marcuspanje@gmail.com>
2017-03-02 21:54:43 -0800

    freq domain is faster

--- depth_estimation.m
+++ depth_estimation.m
@@ -29,13 +29,8 @@ for i = 1:nViews
 end
  
 %size of patch
-szP = 3; %should be odd
-cP_2 = (szP-1)/2;
-
-Prows = 1:szP:sz_lf(1);
-Prows = Prows(1:end-1);
-Pcols = 1:szP:sz_lf(2); 
-Pcols = Pcols(1:end-1);
+szP = 5; %should be odd
+FK = psf2otf(ones(szP), sz_lf(1:2));
 
 h = zeros(sz_lf(1:2));
 
@@ -44,39 +39,32 @@ Ix = zeros(sz_lf(1), sz_lf(2), 1, 1, 3);
 Iy = zeros(sz_lf(1), sz_lf(2), 1, 1, 3); 
 Ix(1:end-1,:,:,:,:) = lf(2:end,:,nV_2, nV_2,:) - lf(1:end-1,:,nV_2, nV_2,:);
 Iy(:,1:end-1,:,:,:) = lf(:,2:end,nV_2, nV_2,:) - lf(:,1:end-1,nV_2, nV_2,:);
-Ix2_Iy2 = Ix.^2 + Iy.^2;
+K_Ix2_Iy2 = ifft2(FK.*fft2(Ix.^2 + Iy.^2));
 
 
-diffV = lf - repmat(lf(:,:,nV_2, nV_2,:),1,1,nViews,nViews,1);
+diffV = lf - lf(:,:,nV_2,nV_2,:);
 Ix_diffV = diffV .* Ix;
 Iy_diffV = diffV .* Iy;
 
 
 disp('compute optical flow');
 %compute optical flow about center image
-for x = cP_2+1:sz_lf(1)-cP_2-1
-  for y = cP_2+1:sz_lf(2)-cP_2-1 
-  %for each pixel, form surrounding patch
-    rows = x-cP_2:x+cP_2;
-    cols = y-cP_2:y+cP_2;
-    num = 0;
-    den = 0;
-    for vx = 1:nViews
-      for vy = 1:nViews
-        if ~valid(vx, vy)
-          continue;
-        end
-          numx = Ix_diffV(rows,cols,vx,vy,:)./(dvx(vx) + (dvx(vx) == 0));
-          numy = Iy_diffV(rows,cols,vx,vy,:)./(dvy(vy) + (dvy(vy) == 0));
-          num = num + sum(numx(:)) + sum(numy(:));
-          den1 = Ix2_Iy2(rows,cols,:);
-          den = den + sum(den1(:));
+Fnum = zeros(sz_lf(1:2));
+den = zeros(sz_lf(1:2));
+for c = 1:3
+  for vx = 1:nViews
+    for vy = 1:nViews
+      if ~valid(vx,vy)
+        continue;
       end
+      Fnum = Fnum + FK.*fft2(Ix_diffV(:,:,vx,vy,c)./(dvx(vx) + (dvx(vx) == 0)));
+      Fnum = Fnum + FK.*fft2(Iy_diffV(:,:,vx,vy,c)./(dvy(vy) + (dvy(vy) == 0)));
+      den = den + K_Ix2_Iy2(:,:,c);
     end
-    h(x,y) = num/den;
   end
 end
-        
+
+h = ifft2(Fnum)./den;
 
 plotter
 
--- plotter.m
+++ plotter.m
@@ -1,13 +1,8 @@
 %scale and display h
-maxVal = 0.2;
-hsc = zeros(size(h));
-hsc(h>0) = max(h(h>0),maxVal);
-hsc(h<0) = min(h(h<0),-maxVal);
-z = 1./hsc;
 
-z = h;
+z = real(h);
 zdisp = z-min(min(z));
-zdisp = 1- (zdisp./max(max(zdisp)));
+zdisp = 1-(zdisp./max(max(zdisp)));
 figure(1);
 imshow(zdisp);
         
