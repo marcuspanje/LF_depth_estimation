776398363dc68395d05fea6a8b793a39e878466c
marcuspanje <marcuspanje@gmail.com>
2017-03-02 20:35:10 -0800

    precompute gradients

--- depth_estimation.m
+++ depth_estimation.m
@@ -13,6 +13,7 @@ for u = 1:nViews
 end
 %}
 
+
 %indices of shift (centered at middle image)
 %right , down in positive
 nViews = 14;
@@ -38,35 +39,44 @@ Pcols = Pcols(1:end-1);
 
 h = zeros(sz_lf(1:2));
 
+disp('precompute gradients');
 Ix = zeros(sz_lf(1), sz_lf(2), 1, 1, 3);
-Iy = zeros(sz_lf(1), sz_lf(2), 1, 2, 3); 
+Iy = zeros(sz_lf(1), sz_lf(2), 1, 1, 3); 
+Ix(1:end-1,:,:,:,:) = lf(2:end,:,nV_2, nV_2,:) - lf(1:end-1,:,nV_2, nV_2,:);
+Iy(:,1:end-1,:,:,:) = lf(:,2:end,nV_2, nV_2,:) - lf(:,1:end-1,nV_2, nV_2,:);
+Ix2_Iy2 = Ix.^2 + Iy.^2;
+
+
+diffV = lf - repmat(lf(:,:,nV_2, nV_2,:),1,1,nViews,nViews,1);
+Ix_diffV = diffV .* Ix;
+Iy_diffV = diffV .* Iy;
+
+
+disp('compute optical flow');
 %compute optical flow about center image
 for x = cP_2+1:sz_lf(1)-cP_2-1
   for y = cP_2+1:sz_lf(2)-cP_2-1 
-    %for each pixel, form surrounding patch
+  %for each pixel, form surrounding patch
     rows = x-cP_2:x+cP_2;
     cols = y-cP_2:y+cP_2;
-    center = lf(rows,cols,nV_2,nV_2,:);
-    Iy = lf(rows,cols+1,nV_2,nV_2,:) - center;
-    Ix = lf(rows+1,cols,nV_2,nV_2,:) - center;
-    den1 = Ix.^2 + Iy.^2;
-    den1 = sum(den1(:));
-    den = 0;
     num = 0;
+    den = 0;
     for vx = 1:nViews
       for vy = 1:nViews
-        %offset from center
-        if valid(vx,vy)%some pictures like image edges are invalid
-          Ivx = (lf(rows,cols,vx,vy,:) - center)./(dvx(vx) + (dvx(vx) == 0));
-          Ivy = (lf(rows,cols,vx,vy,:) - center)./(dvy(vy) + (dvy(vy)==0));
-          num1 = Ix.*Ivx + Iy.*Ivy;
-          num = num + sum(num1(:));
-          den = den + den1;
+        if ~valid(vx, vy)
+          continue;
         end
+          numx = Ix_diffV(rows,cols,vx,vy,:)./(dvx(vx) + (dvx(vx) == 0));
+          numy = Iy_diffV(rows,cols,vx,vy,:)./(dvy(vy) + (dvy(vy) == 0));
+          num = num + sum(numx(:)) + sum(numy(:));
+          den1 = Ix2_Iy2(rows,cols,:);
+          den = den + sum(den1(:));
       end
     end
     h(x,y) = num/den;
   end
 end
+        
 
 plotter
+
