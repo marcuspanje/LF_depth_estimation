20d9facfeff5a1b2d2d27d47387d60af90a1038b
marcuspanje <marcuspanje@gmail.com>
2017-03-05 23:54:09 -0800

    added proper inverse scaling

--- plotter.m
+++ plotter.m
@@ -1,17 +1,32 @@
 %scale and display h
-zdisp = real(ifft2(Fx));
-%zdisp(z<0) = 0;
-zdisp = zdisp-min(min(zdisp));
-maxZ = max(zdisp(:));
-zdisp = 1-(zdisp./maxZ);
+z = real(ifft2(Fx));
+min_th = 20;
+zpos = z(z > min_th);
+mu = mean(zpos); sig = std(zpos);
+max_th = mu + sig 
+min_th = mu - sig 
+zdisp = max(z, min_th);
+zdisp = min(zdisp, max_th);
+zdisp = (1./zdisp);
+zdisp = zdisp./max(zdisp(:));
 figure(1);
+subplot(1,3,1);
 imshow(zdisp);
+title('OF map with admm');
 
-h(h<0) = 0;
-hdisp = h - min(h(:));
-hdisp = 1 - (hdisp ./ max(hdisp(:)));
-figure(2);
+hpos = h(h>min_th);
+mu = mean(hpos); sig = std(hpos);
+hdisp = max(h, min_th);
+hdisp = min(hdisp, max_th);
+hdisp = 1./hdisp;
+hdisp = hdisp ./ max(hdisp(:));
+subplot(1,3,2);
 imshow(hdisp);
+title('OF map');
+
+subplot(1,3,3);
+imshow(squeeze(lf(:,:,nV_2,nV_2,:)));
+title('original');
         
      
 
